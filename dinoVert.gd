7d1c219f2d4dee3a1d2e73668524935c13a91468
= <bandosio@gmail.com>
2023-11-15 11:19:20 +0100

    initial commit2

---
+++ dinoVert.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@onready var _animation_player = $AnimatedSprite2D
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+
+func _ready():
+	_animation_player.play("idle")
+
+func _process(_delta):
+	if Input.is_action_pressed("right"):
+		_animation_player.play("walk")
+	else:
+		_animation_player.stop()
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("jump") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("left", "right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
